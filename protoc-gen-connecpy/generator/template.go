100de47ba0b33db759f24af7c575d36cf3ed93ee
Yasushi Itoh <i2y@icloud.com>
2024-06-16 15:11:42 +0900

    chore: format the template code and existing code
    - update rye and format existing code using it
    - format the template code

--- protoc-gen-connecpy/generator/template.go
+++ protoc-gen-connecpy/generator/template.go
@@ -48,45 +48,45 @@ import {{.ModuleName}}_pb2 as _pb2
 
 {{range .Services}}
 class {{.Name}}Service(Protocol):{{- range .Methods }}
-	async def {{.Name}}(self, req: _pb2.{{.InputType}}, ctx: ServiceContext) -> _pb2.{{.OutputType}}:
-		...{{- end }}
+    async def {{.Name}}(self, req: _pb2.{{.InputType}}, ctx: ServiceContext) -> _pb2.{{.OutputType}}: ...
+{{- end }}
 
 
 class {{.Name}}Server(ConnecpyServer):
-	def __init__(self, *, service: {{.Name}}Service, server_path_prefix=""):
-		super().__init__(service=service)
-		self._prefix = f"{server_path_prefix}/{{.ServiceURL}}"
-		self._endpoints = { {{- range .Methods }}
-			"{{.Name}}": Endpoint[_pb2.{{.InputType}}, _pb2.{{.OutputType}}](
-				service_name="{{.ServiceName}}",
-				name="{{.Name}}",
-				function=getattr(service, "{{.Name}}"),
-				input=_pb2.{{.InputType}},
-				output=_pb2.{{.OutputType}},
-			),{{- end }}
-		}
+    def __init__(self, *, service: {{.Name}}Service, server_path_prefix=""):
+        super().__init__(service=service)
+        self._prefix = f"{server_path_prefix}/{{.ServiceURL}}"
+        self._endpoints = { {{- range .Methods }}
+            "{{.Name}}": Endpoint[_pb2.{{.InputType}}, _pb2.{{.OutputType}}](
+                service_name="{{.ServiceName}}",
+                name="{{.Name}}",
+                function=getattr(service, "{{.Name}}"),
+                input=_pb2.{{.InputType}},
+                output=_pb2.{{.OutputType}},
+            ),{{- end }}
+        }
 
 
 class {{.Name}}Client(ConnecpyClient):{{range .Methods}}
-	def {{.Name}}(
-		self,
-		*,
-		request,
-		ctx,
-		server_path_prefix="",
-		**kwargs,
-	):
-		return self._make_request(
-			url=f"{server_path_prefix}/{{.ServiceURL}}/{{.Name}}",
-			ctx=ctx,
-			request=request,
-			response_obj=_pb2.{{.OutputType}},
-			**kwargs,
-		){{end}}
+    def {{.Name}}(
+        self,
+        *,
+        request,
+        ctx,
+        server_path_prefix="",
+        **kwargs,
+    ):
+        return self._make_request(
+            url=f"{server_path_prefix}/{{.ServiceURL}}/{{.Name}}",
+            ctx=ctx,
+            request=request,
+            response_obj=_pb2.{{.OutputType}},
+            **kwargs,
+        ){{end}}
 
 
 class Async{{.Name}}Client(AsyncConnecpyClient):{{range .Methods}}
-	async def {{.Name}}(
+    async def {{.Name}}(
         self,
         *,
         request,
@@ -95,12 +95,12 @@ class Async{{.Name}}Client(AsyncConnecpyClient):{{range .Methods}}
         session: Union[httpx.AsyncClient, None] = None,
         **kwargs,
     ):
-		return await self._make_request(
-			url=f"{server_path_prefix}/{{.ServiceURL}}/{{.Name}}",
-			ctx=ctx,
-			request=request,
-			response_obj=_pb2.{{.OutputType}},
-			session=session,
-			**kwargs,
-		)
+        return await self._make_request(
+            url=f"{server_path_prefix}/{{.ServiceURL}}/{{.Name}}",
+            ctx=ctx,
+            request=request,
+            response_obj=_pb2.{{.OutputType}},
+            session=session,
+            **kwargs,
+        )
 {{end}}{{end}}`))
