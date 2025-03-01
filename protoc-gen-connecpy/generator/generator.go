e7132d9f5c0e68944be3fa79017a0360f55db47f
Yasushi Itoh <6240399+i2y@users.noreply.github.com>
2025-03-01 15:30:32 +0900

    fmt

--- protoc-gen-connecpy/generator/generator.go
+++ protoc-gen-connecpy/generator/generator.go
@@ -16,7 +16,12 @@ import (
 
 func Generate(r *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
 	resp := &plugin.CodeGeneratorResponse{}
+
 	resp.SupportedFeatures = proto.Uint64(uint64(plugin.CodeGeneratorResponse_FEATURE_PROTO3_OPTIONAL) | uint64(plugin.CodeGeneratorResponse_FEATURE_SUPPORTS_EDITIONS))
+	edition_2023 := int32(descriptorpb.Edition_EDITION_2023)
+	edition_proto3 := int32(descriptorpb.Edition_EDITION_PROTO3)
+	resp.MinimumEdition = &edition_proto3
+	resp.MaximumEdition = &edition_2023
 
 	files := r.GetFileToGenerate()
 	if len(files) == 0 {
@@ -50,12 +55,6 @@ func Generate(r *plugin.CodeGeneratorRequest) *plugin.CodeGeneratorResponse {
 		resp.File = append(resp.File, connecpyFile)
 		return true
 	})
-
-	// Add Edition support
-	edition_proto3 := int32(descriptorpb.Edition_EDITION_PROTO3)
-	edition_2023 := int32(descriptorpb.Edition_EDITION_2023)
-	resp.MinimumEdition = &edition_proto3
-	resp.MaximumEdition = &edition_2023
 
 	return resp
 }
