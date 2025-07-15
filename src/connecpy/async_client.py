2bd87a499ef2623c374752b91de3964ede48756a
Anuraag (Rag) Agrawal <anuraaga@gmail.com>
2025-07-15 19:58:13 +0900

    fix: rework client timeouts to work closer to connect (#48)

--- src/connecpy/async_client.py
+++ src/connecpy/async_client.py
@@ -1,7 +1,10 @@
+from asyncio import wait_for
 from typing import Optional, TypeVar
-import httpx
 
+import httpx
 from google.protobuf.message import Message
+from httpx import Timeout
+
 from . import shared_client
 from . import compression
 from . import context
@@ -19,19 +22,26 @@ class AsyncConnecpyClient:
 
     Args:
         address (str): The address of the Connecpy server.
-        session (httpx.AsyncClient): The httpx client session to use for making requests.
+        timeout_ms (int): The timeout in ms for the overall request.
+        session (httpx.AsyncClient): The httpx client session to use for making requests. If setting timeout_ms,
+            the session should have timeout disabled or set higher than timeout_ms.
     """
 
     def __init__(
-        self, address: str, timeout=5, session: Optional[httpx.AsyncClient] = None
+        self,
+        address: str,
+        timeout_ms: Optional[int] = None,
+        session: Optional[httpx.AsyncClient] = None,
     ) -> None:
         self._address = address
-        self._timeout = timeout
+        self._timeout_ms = timeout_ms
         if session:
             self._session = session
             self._close_client = False
         else:
-            self._session = httpx.AsyncClient()
+            self._session = httpx.AsyncClient(
+                timeout=_convert_connect_timeout(timeout_ms)
+            )
             self._close_client = True
         self._closed = False
 
@@ -56,6 +66,7 @@ class AsyncConnecpyClient:
         ctx: Optional[context.ClientContext],
         response_class: type[_RES],
         method="POST",
+        timeout_ms: Optional[int] = None,
         session: Optional[httpx.AsyncClient] = None,
         **kwargs,
     ) -> _RES:
@@ -79,7 +90,13 @@ class AsyncConnecpyClient:
             exceptions.ConnecpyServerException: If an error occurs while making the request.
         """
         # Prepare headers and kwargs using shared logic
-        headers, kwargs = shared_client.prepare_headers(ctx, kwargs, self._timeout)
+        if timeout_ms is None:
+            timeout_ms = self._timeout_ms
+        else:
+            timeout = _convert_connect_timeout(timeout_ms)
+            kwargs["timeout"] = timeout
+        headers, kwargs = shared_client.prepare_headers(ctx, kwargs, timeout_ms)
+        timeout_s = timeout_ms / 1000.0 if timeout_ms is not None else None
 
         try:
             client = session or self._session
@@ -95,10 +112,18 @@ class AsyncConnecpyClient:
                 params = shared_client.prepare_get_params(request_data, headers)
                 kwargs["params"] = params
                 kwargs["headers"].pop("content-type", None)
-                resp = await client.get(url=self._address + url, **kwargs)
+                resp = await wait_for(
+                    client.get(url=self._address + url, **kwargs),
+                    timeout_s,
+                )
             else:
-                resp = await client.post(
-                    url=self._address + url, content=request_data, **kwargs
+                resp = await wait_for(
+                    client.post(
+                        url=self._address + url,
+                        content=request_data,
+                        **kwargs,
+                    ),
+                    timeout_s,
                 )
 
             if resp.status_code == 200:
@@ -112,10 +137,10 @@ class AsyncConnecpyClient:
                 return response
             else:
                 raise ConnectWireError.from_response(resp).to_exception()
-        except httpx.TimeoutException as e:
+        except (httpx.TimeoutException, TimeoutError):
             raise exceptions.ConnecpyServerException(
                 code=errors.Errors.DeadlineExceeded,
-                message=str(e) or "request timeout",
+                message="Request timed out",
             )
         except exceptions.ConnecpyException:
             raise
@@ -124,3 +149,13 @@ class AsyncConnecpyClient:
                 code=errors.Errors.Unavailable,
                 message=str(e),
             )
+
+
+def _convert_connect_timeout(timeout_ms: Optional[int]) -> Timeout:
+    if timeout_ms is None:
+        # If no timeout provided, match connect-go's default behavior of a 30s connect timeout
+        # and no read/write timeouts.
+        return Timeout(None, connect=30.0)
+    # We apply the timeout to the entire operation per connect's semantics so don't need
+    # HTTP timeout
+    return Timeout(None)
