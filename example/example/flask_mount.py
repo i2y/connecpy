f8387bcaa7cee8894bfd0b27f84ae32f4c065743
Anuraag Agrawal <anuraaga@gmail.com>
2025-07-18 13:05:31 +0900

    git add

---
+++ example/example/flask_mount.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from .wsgi_service import HaberdasherService
+from .haberdasher_connecpy import HaberdasherWSGIApplication
+
+haberdasher_app = HaberdasherWSGIApplication(HaberdasherService())
+
+app = Flask(__name__)
+
+
+@app.route("/healthz")
+def health():
+    return "OK"
+
+
+app.wsgi_app = DispatcherMiddleware(
+    app.wsgi_app,
+    {
+        haberdasher_app.path: haberdasher_app,
+    },
+)
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=3000, debug=True)
---
+++ example/example/routing_asgi.py
@@ -0,0 +1,44 @@
+from typing import cast
+
+from starlette.applications import Starlette
+from starlette.responses import PlainTextResponse
+from starlette.routing import Mount, Route
+from starlette.types import ASGIApp
+
+from .haberdasher_connecpy import HaberdasherASGIApplication
+from .service import HaberdasherService
+
+haberdasher_app = HaberdasherASGIApplication(HaberdasherService())
+
+
+def strip_prefix(prefix: str, app: ASGIApp) -> ASGIApp:
+    async def stripped_app(scope, receive, send):
+        scope["path"] = scope["path"].removeprefix(prefix)
+        return await app(scope, receive, send)
+
+    return stripped_app
+
+
+app = Starlette(
+    routes=[
+        Route(
+            "/healthz",
+            lambda _: PlainTextResponse("OK"),
+        ),
+        Mount(
+            "/services",
+            app=cast(ASGIApp, haberdasher_app),
+        ),
+        Mount(
+            "/moreservices",
+            app=Mount(
+                haberdasher_app.path,
+                app=strip_prefix("/moreservices", cast(ASGIApp, haberdasher_app)),
+            ),
+        ),
+        Mount(
+            haberdasher_app.path,
+            app=cast(ASGIApp, haberdasher_app),
+        ),
+    ]
+)
