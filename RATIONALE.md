c4d3d0f72d630f8c4d8f40fcf4659fe4b10fe1e3
Anuraag Agrawal <anuraaga@gmail.com>
2025-08-21 11:21:00 +0900

    chore: add script to generate wheels for protoc-gen-connecpy

---
+++ RATIONALE.md
@@ -0,0 +1,10 @@
+# Connecpy Rationale
+
+Much of the design of Connecpy is based on the principle of providing the best
+experience we can think of for the 99% use case, while also supporting the
+rest, possibly through more somewhat more complicated means. This number refers
+to usages of Connecpy, not users, as even more advanced use cases will
+generally be a subset of usage for even a single user. So we think this
+philosophy is a win for everybody.
+
+Below are documentation of notable design decisions in this library that may
---
+++ protoc-gen-connecpy/scripts/generate_wheels.py
@@ -0,0 +1,55 @@
+import json
+import shutil
+import subprocess
+from pathlib import Path
+
+
+def main() -> None:
+    base_dir = Path(__file__).parent / ".." / ".."
+    with (base_dir / "out" / "artifacts.json").open() as f:
+        artifacts: list[dict[str, str]] = json.load(f)
+    for artifact in artifacts:
+        if artifact["type"] != "Binary":
+            continue
+        # Check https://go.dev/wiki/MinimumRequirements#operating-systems for
+        # minimum OS versions
+        platform = ""
+        match artifact["goos"]:
+            case "darwin":
+                match artifact["goarch"]:
+                    case "amd64":
+                        platform = "macosx_11_0_x86_64"
+                    case "arm64":
+                        platform = "macosx_11_0_arm64"
+            case "linux":
+                # While manylinux1 is considered legacy versus the more
+                # precise manylinux_x_y, our binaries are statically compiled
+                # so we go with it for maximum compatibility.
+                match artifact["goarch"]:
+                    case "amd64":
+                        platform = "manylinux1_x86_64"
+                    case "arm64":
+                        platform = "manylinux1_aarch64"
+            case "windows":
+                match artifact["goarch"]:
+                    case "amd64":
+                        platform = "win_amd64"
+                    case "arm64":
+                        platform = "win_arm64"
+        if not platform:
+            msg = f"Unsupported platform: {artifact['goos']}/{artifact['goarch']}"
+            raise ValueError(msg)
+        exe_path = base_dir / artifact["path"]
+        bin_dir = Path(__file__).parent / ".." / "out" / "bin"
+        shutil.rmtree(bin_dir, ignore_errors=True)
+        bin_dir.mkdir(parents=True, exist_ok=True)
+        shutil.copyfile(exe_path, bin_dir / exe_path.name)
+        subprocess.run(["uv", "build", "--wheel"], check=True)  # noqa: S607
+        dist_dir = Path(__file__).parent / ".." / "dist"
+        wheel = next(dist_dir.glob("*-py3-none-any.whl"))
+        base_name = wheel.name[: -len("-py3-none-any.whl")]
+        wheel.rename(dist_dir / f"{base_name}-py3-none-{platform}.whl")
+
+
+if __name__ == "__main__":
+    main()
